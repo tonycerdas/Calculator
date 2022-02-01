62e7b3e1c9e8ee0a063db9006d6f1a332655c691
Anthony <anthonycerdas51@gmail.com>
2022-02-01 11:24:02 -0600

    Number keyboard handling: Update

--- main.js
+++ main.js
@@ -16,15 +16,6 @@ function calculate() {
   screen.innerHTML = y;
 }
 
-screen.addEventListener("keypress", handlekeypress, false);
-
-var handlekeypress = function (event) {
-  var asciiCode = event.key ? event.key : event.charCode;
-  var asciiValue = String.fromCharCode(asciiCode);
-  display(value);
-  return false;
-};
-
 document.addEventListener("keydown", function(event) {
   display(event.key);
 });
